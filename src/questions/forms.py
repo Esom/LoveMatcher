5a7a31fa8d3a6ca281f3aecfd3f5bfc36c668270
esom <mailesom@gmail.com>
2016-05-31 16:06:07 +0100

    Fixed useranswer model bug

--- src/questions/forms.py
+++ src/questions/forms.py
@@ -1,14 +1,9 @@
 from django import forms
+from .models import LEVELS
 
-LEVELS = (
-	('Mandatory', 'Mandatory'),
-	('Very Important', 'Very Important'),
-	('Somewhat Important', 'Somewhat Important'),
-	('Not Important', 'Not Important	')
-	)
 class UserResponseForm(forms.Form):
 	question_id  =  forms.IntegerField()
 	answer_id = forms.IntegerField()
-	coworker_answer_id	= forms.IntegerField()
+	their_answer_id	= forms.IntegerField()
 	importance_level = forms.ChoiceField(choices=LEVELS)
-	coworker_importance_level = forms.ChoiceField(choices=LEVELS)
+	their_importance_level = forms.ChoiceField(choices=LEVELS)
--- src/questions/views.py
+++ src/questions/views.py
@@ -4,18 +4,39 @@ from django.http import Http404
 # Create your views here.
 
 from .forms import UserResponseForm
-from .models import Question, Answer
+from .models import Question, Answer, UserAnswer
 
 # Create your views here.
 def single(request,id):
 	if request.user.is_authenticated():
 		form = UserResponseForm(request.POST or None)
 		if form.is_valid():
-			question_id = form.cleaned_data.get('question_id')
-			answer_id =  form.cleaned_data.get('answer_id')
+			print form.cleaned_data
+			#print request.POST
+
+			question_id = form.cleaned_data.get('question_id') #form.cleaned_data['question_id']
+			answer_id = form.cleaned_data.get('answer_id')
+			importance_level = form.cleaned_data.get('importance_level')
+
+			their_importance_level = form.cleaned_data.get('their_importance_level')
+			their_answer_id = form.cleaned_data.get('their_answer_id')
+
 			question_instance = Question.objects.get(id=question_id)
 			answer_instance = Answer.objects.get(id=answer_id)
-			print question_instance.text, answer_instance.text
+
+			new_user_answer = UserAnswer()
+			new_user_answer.user = request.user
+			new_user_answer.question = question_instance
+			new_user_answer.my_answer = answer_instance
+			new_user_answer.my_answer_importance = importance_level
+
+			if their_answer_id != -1:
+				their_answer_istance = Answer.objects.get(id=their_answer_id)
+				new_user_answer.their_answer = their_answer_istance
+				new_user_answer.their_importance = their_importance_level
+			else:
+				new_user_answer.their_importance = "Not Important"
+			new_user_answer.save()
 			next_q = Question.objects.all().order_by("?").first()
 			return redirect("question_single",id=next_q.id)
 
@@ -24,7 +45,7 @@ def single(request,id):
 		instance = get_object_or_404(Question,id=id)
 		context = {"form":form,"instance":instance,}
 
-		return render(request,"questions/home.html",context) 
+		return render(request,"questions/single.html",context) 
 	else:
 		raise Http404
 
